471c21fafa37c6889ce83091069fb342f7011ead
tomashm01 <tomashidalgomartin@gmail.com>
2023-12-24 22:56:25 +0100

    feat: :zap: dashboards stats

--- apps/web/pages/dashboard/components/adminDashboard.tsx
+++ apps/web/pages/dashboard/components/adminDashboard.tsx
@@ -1,5 +1,37 @@
+import { Box, Container, Heading, Stat, StatHelpText, StatLabel, StatNumber } from "@chakra-ui/react";
+
+const KPI = ({ label, value, helpText }:any) => (
+  <Stat p={5} shadow="md" border="1px" borderColor="gray.200">
+    <StatLabel>{label}</StatLabel>
+    <StatNumber>{value}</StatNumber>
+    <StatHelpText>{helpText}</StatHelpText>
+  </Stat>
+);
 const AdminDashboard = () => {
-  return <h1>Admin Dashboard</h1>;
+  return (
+    <Container maxW="container.xl">
+      <Heading as="h1" size="xl" my={5}>
+        Dashboard de admin
+      </Heading>
+      <Box display="flex" justifyContent="space-around">
+        <KPI
+          label="Ingresos al Mes"
+          value="₹50,000" // Sustituye esto por tus datos
+          helpText="Comparado al mes pasado"
+        />
+        <KPI
+          label="Tiempo Medio de Reparación"
+          value="2 horas"
+          helpText="Tiempo promedio este mes"
+        />
+        <KPI
+          label="Clientes Nuevos al Mes"
+          value="35"
+          helpText="20% más que el mes pasado"
+        />
+      </Box>
+    </Container>
+  );
 };
 
 export default AdminDashboard;
--- apps/web/pages/dashboard/components/techDashboard.tsx
+++ apps/web/pages/dashboard/components/techDashboard.tsx
@@ -1,5 +1,32 @@
+import { Box, Container, Heading, Stat, StatHelpText, StatLabel, StatNumber } from "@chakra-ui/react";
+
+const KPI = ({ label, value, helpText }:any) => (
+  <Stat p={5} shadow="md" border="1px" borderColor="gray.200">
+    <StatLabel>{label}</StatLabel>
+    <StatNumber>{value}</StatNumber>
+    <StatHelpText>{helpText}</StatHelpText>
+  </Stat>
+);
 const TechDashboard = () => {
-  return <h1>Tech Dashboard</h1>;
+  return (
+    <Container maxW="container.xl">
+      <Heading as="h1" size="xl" my={5}>
+        Dashboard de técnico
+      </Heading>
+      <Box display="flex" justifyContent="space-around">
+        <KPI
+          label="Tiempo Medio de Reparación"
+          value="1.5 horas"
+          helpText="Tiempo promedio este mes"
+        />
+        <KPI
+          label="Número de reparaciones al Mes"
+          value="20"
+          helpText="20% más que el mes pasado"
+        />
+      </Box>
+    </Container>
+  );
 };
 
 export default TechDashboard;
